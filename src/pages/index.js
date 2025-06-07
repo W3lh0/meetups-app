5a85f063f81f05379ed2e9c5302d67025c25fb46
W3lh0 <e2101506@edu.vamk.fi>
2025-06-07 11:36:06 +0300

    feat: fetch function for data fetching from database

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,25 @@ function HomePage(props) {
 
 export async function getStaticProps() {
   // fetch data from an API
+  const client = await MongoClient.connect(
+    process.env.MONGODB_URL
+  )
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 1
   }; 
