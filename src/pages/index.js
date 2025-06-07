e7c1e1fa55d0ba5e22781f107305fc0b24e0da66
W3lh0 <e2101506@edu.vamk.fi>
2025-06-07 10:20:01 +0300

    feat: deleted the dummy data and added import for MongoClient

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,5 @@
+import { MongoClient } from 'mongodb';
 import MeetupList from '../components/meetups/MeetupList';
-
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup: Vaskiluoto',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/0/02/Vasklot_hamn_cirka_1994_med_Silja_Festival_p%C3%A5_inkommande_och_Wasa_Queen_vid_kaj_i_bakgrunden_Sundom_sk%C3%A4rg%C3%A5rd.jpg',
-    address: 'Vaskiluoto, Vaasa',
-    description: 'This is a first meetup! Picture: Maritime Museum of Finland, CC BY-SA 4.0',
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup: VAMK',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/8/88/Vaasan_ammattikorkeakoulu_2018.jpg',
-    address: 'Wolffintie 30, Vaasa',
-    description: 'This is a second meetup! Picture: Santeri Viinamäki, 2018m CC BY-SA 4.0',
-  },
-];
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
