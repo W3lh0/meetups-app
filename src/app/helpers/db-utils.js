fb6fe7fb03fcfab18b13712231576638116218f5
W3lh0 <e2101506@edu.vamk.fi>
2025-06-12 17:02:24 +0300

    feat: Implement homepage meetup fetching with db-utils

--- src/app/helpers/db-utils.js
+++ src/app/helpers/db-utils.js
@@ -1,9 +1,8 @@
 // src/helpers/db-utils.js
 import { MongoClient } from "mongodb";
-import { cache } from "react";
 
 const MONGODB_URL = process.env.MONGODB_URL;
-const MONGODB_DB_NAME = process.env.MONGODB_DB;
+const MONGODB_DB_NAME = process.env.MONGODB_DB_NAME;
 
 if (!MONGODB_URL) {
     throw new Error('Please define the MONGODB_URL enviroment veriable inside .env.local');
@@ -16,7 +15,7 @@ if (!MONGODB_DB_NAME) {
 let cachedClient = null;
 let cachedDb = null;
 
-async function connectToDatabase() {
+export async function connectToDatabase() {
     if (cachedClient && cachedDb) {
         console.log('Using cached database connection.');
         return { client: cachedClient, db: cachedDb };
@@ -39,7 +38,7 @@ async function connectToDatabase() {
     }
 }
 
-export async function insertDogument(client, collection, document) {
+export async function insertDocument(client, collection, document) {
     try {
         const db = client.db(MONGODB_DB_NAME);
         const result = await db.collection(collection).insertOne(document);
@@ -51,7 +50,7 @@ export async function insertDogument(client, collection, document) {
     }
 }
 
-export async function getAllDocument(client, collection, sort, filter = {}) {
+export async function getAllDocuments(client, collection, sort, filter = {}) {
     try {
         const db = client.db(MONGODB_DB_NAME);
         const document = await db.collection(collection).find(filter).sort(sort).toArray();
