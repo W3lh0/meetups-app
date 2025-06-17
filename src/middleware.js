75d79ba511390d48116bbcfc3959edc5cf1e71d5
W3lh0 <e2101506@edu.vamk.fi>
2025-06-17 09:58:58 +0300

    fix(middleware.js): fiexed macher

--- src/middleware.js
+++ src/middleware.js
@@ -7,6 +7,6 @@ export async function middleware(request) {
 
 export const config = {
     matcher: [
-        '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|/auth).*)'
+        '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|).*)'
     ],
 };
