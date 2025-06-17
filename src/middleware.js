612680f228b9ec71c3565f4c6283da8401b6db5d
W3lh0 <e2101506@edu.vamk.fi>
2025-06-17 09:51:02 +0300

    fix: middleware pathing fix

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,5 @@
 import { auth0 } from '@/lib/auth0';
+import { withMiddlewareAuthRquired} from '@auth0/nextjs-auth0/middleware'
 
 export async function middleware(request) {
     console.log('Middleware called for', request.url);
@@ -7,6 +8,6 @@ export async function middleware(request) {
 
 export const config = {
     matcher: [
-        '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|api/auth).*)'
+        '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|/auth).*)'
     ],
 };
