ecd725f4104c2cb309e1810aafdcab2f7248fda7
W3lh0 <e2101506@edu.vamk.fi>
2025-06-19 13:16:56 +0300

    feat(auth0): fix Vercel URL handling and update middleware

--- src/lib/auth0.js
+++ src/lib/auth0.js
@@ -1,9 +1,11 @@
 import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
+const baseURL = process.env.APP_BASE_URL || (process.env.VERCEL_URL ? `https://${VERCEL_URL}` : 'https://localhost:3000');
+
 export const auth0 = new Auth0Client({
     secret: process.env.AUTH0_SECRET,
-    baseURL: process.env.APP_BASE_URL,
-    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+    baseURL,
+    issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
 });
