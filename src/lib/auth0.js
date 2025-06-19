29d4263246b428c4a62a39db4d43ed2c6358f450
W3lh0 <e2101506@edu.vamk.fi>
2025-06-19 20:30:57 +0300

    refactor(auth0): Refactor back to original form

--- src/lib/auth0.js
+++ src/lib/auth0.js
@@ -1,20 +1,3 @@
 import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
-const ensureProtocol = (url) => {
-    if (!url) return 'http://localhost:3000';
-    return url.startsWith('http://') || url.startsWith('https://') ? url : `https://${url}`;
-}
-
-const baseURL = ensureProtocol(process.env.APP_BASE_URL) || (process.env.VERCEL_URL || 'https://localhost:3000');
-
-export const auth0 = new Auth0Client({
-    secret: process.env.AUTH0_SECRET,
-    baseURL,
-    issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
-    clientID: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-});
-
-if (typeof window === 'undefined' && process.env.VERCEL_URL) {
-    auth0.baseURL === ensureProtocol(process.env.VERCEL_URL);
-}
+export const auth0 = new Auth0Client({});
